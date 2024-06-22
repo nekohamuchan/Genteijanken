8381fb396b8585a6423b05ee4f9e55850813574b
Hsin Yi Huang <151099887+nekohamuchan@users.noreply.github.com>
2024-06-22 19:51:55 +0800

    Create getComputerChoice function

---
+++ script.js
@@ -0,0 +1,20 @@
+const getComputerChoice = () => {
+    const randomInt = Math.floor(Math.random() * 3);
+    let computerChoice = "";
+    
+    switch (randomInt) {
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Scissors"
+            break;
+    }
+
+    return computerChoice;
+}
+
+console.log();
