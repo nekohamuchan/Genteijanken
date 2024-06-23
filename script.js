d06c864eb3585f1c0aefa41da7e89fedf376cc1c
Hsin Yi Huang <151099887+nekohamuchan@users.noreply.github.com>
2024-06-23 18:47:03 +0800

    Create playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let playerScore = 0;
+
 const getComputerChoice = () => {
     const randomInt = Math.floor(Math.random() * 3);
     let computerChoice = "";
@@ -20,6 +23,44 @@ const getComputerChoice = () => {
 const getPlayerChoice = () => {
     const playerChoice = prompt("Your choice?");
     return playerChoice;
+
+    //test code
+    const randomInt = Math.floor(Math.random() * 3);
+    let computerChoice = "";
+    
+    switch (randomInt) {
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Scissors"
+            break;
+    }
+
+    return computerChoice;
 }
 
-console.log();
+const playRound = (playerSelect = getPlayerChoice().toLowerCase(),
+ computerSelect = getComputerChoice().toLowerCase()) => {
+
+    console.log(playerSelect, computerSelect);
+    if ((playerSelect === "rock" && computerSelect === "scissors") || 
+    (playerSelect === "paper" && computerSelect === "rock") || 
+    (playerSelect === "scissors" && computerSelect === "paper")) {
+        playerScore++;
+        return `You win!`;
+    } else if ((playerSelect === "rock" && computerSelect === "paper") || 
+    (playerSelect === "paper" && computerSelect === "scissors") || 
+    (playerSelect === "scissors" && computerSelect === "rock")) {
+        computerScore++;
+        return `You lose!`;
+    } else {
+        return `It's a tie!`;
+    }
+
+}
+
+console.log(playRound());
